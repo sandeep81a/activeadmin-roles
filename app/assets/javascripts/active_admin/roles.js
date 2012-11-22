8cd5ec84a7253fdf8574538161e95a2d00b2a192
Greg Bell <gregdbell@gmail.com>
2012-11-22 13:04:41 -0800

    Added ability to select rows and columns when adding permissions

---
+++ app/assets/javascripts/active_admin/roles.js
@@ -0,0 +1,1 @@
+// Active Admin - Roles
--- spec/dummy/app/assets/javascripts/active_admin.js
+++ spec/dummy/app/assets/javascripts/active_admin.js
@@ -1,1 +1,53 @@
 //= require active_admin/base
+//= require active_admin/roles
+
+(function(AA, $, undefined){
+
+  AA.Roles = {};
+
+  AA.Roles.init = function(){
+    $(".permissions-table .permission-row-title").click(AA.Roles.onClickRowTitle);
+    $(".permissions-table .permission-column-title").click(AA.Roles.onClickColumnTitle);
+  }
+
+  AA.Roles.onClickRowTitle = function(){
+    var inputs = AA.Roles.checkboxesForRow($(this));
+    checkOrUncheckInputs(inputs)
+
+    return false;
+  }
+
+  AA.Roles.onClickColumnTitle = function(){
+    var inputs = AA.Roles.checkboxesForColumn($(this));
+    checkOrUncheckInputs(inputs)
+
+    return false;
+  }
+
+  function isAllChecked(inputs){
+    return inputs.not(":checked").size() == 0
+  }
+
+  function checkOrUncheckInputs(inputs){
+    if(isAllChecked(inputs)) {
+      inputs.attr("checked", false);
+    } else {
+      inputs.attr("checked", true);
+    }
+  }
+
+  AA.Roles.checkboxesForRow = function(heading) {
+    var row = heading.closest("tr");
+    return row.find("input[type=checkbox]");
+  }
+
+  AA.Roles.checkboxesForColumn = function(heading) {
+      var table = heading.closest(".permissions-table");
+      var columnIndex = table.find("th").index(heading);
+
+      return table.find("tr td:nth-child("+(columnIndex+1)+") input[type=checkbox]");
+  }
+
+  $(function(){ AA.Roles.init(); });
+
+})(AA, jQuery);
