f190e36e28a222331fb80fe8fd4f58fbf22e0edc
Philippe Creux <pcreux@gmail.com>
2012-12-06 15:01:27 +0100

    Spec permissions with many roles

--- spec/unit/authorization_adapter_spec.rb
+++ spec/unit/authorization_adapter_spec.rb
@@ -23,6 +23,23 @@ describe ActiveAdmin::Roles::AuthorizationAdapter do
 
     it "should allow access when has permission" do
       adapter.authorized?(:read, AdminUser).should be_true
+      adapter.authorized?(:destroy, AdminUser).should be_false
+    end
+
+  end
+
+  context "with many roles" do
+
+    before do
+      role_1 = ActiveAdmin::Role.new(:name => "Administrators", :permissions => "admin.admin_users.read")
+      role_2 = ActiveAdmin::Role.new(:name => "Creators", :permissions => "admin.admin_users.create")
+      ActiveAdmin::RoleService.new.set_roles(user, [role_1, role_2])
+    end
+
+    it "should allow access when has permission" do
+      adapter.authorized?(:read, AdminUser).should be_true
+      adapter.authorized?(:create, AdminUser).should be_true
+      adapter.authorized?(:destroy, AdminUser).should be_false
     end
 
   end
